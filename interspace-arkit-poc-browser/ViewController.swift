11481ddde15af412e4c135beffec3f3f561b9e1f
Ben Woodley <benwoodley@Bens-MBP-3.qs.unipart.io>
2021-04-02 20:14:56 +0100

    Rewrote the whole ViewController from scratch so so it makes more sense to read, is less crash prone, is generally a less hacky mess of swift-incompetence. Also put a comment above every line describing the logic taking place, so it can be read by anyone including me when Ive forgotten every line. I havent included the unused function that creates an anchor from the client and pushes it with a POST, it isnt neeeded yet anyway. But yea, all works as it did yesterday again.@

--- interspace-arkit-poc-browser/ViewController.swift
+++ interspace-arkit-poc-browser/ViewController.swift
@@ -1,162 +1,166 @@
 //
-//  ViewController.swift
+//  ViewControllerNew.swift
 //  interspace-arkit-poc-browser
 //
-//  Created by Ben Woodley on 30/03/2021.
+//  Created by Ben Woodley on 02/04/2021.
 //
+
 import ARKit
 import RealityKit
 
-struct Vector3: Codable {
+// For decoding JSON text storing position
+struct Vec3: Codable {
     let x: Float
     let y: Float
     let z: Float
 }
-
-struct Vector4: Codable {
+// For decoding JSON text storing orientation
+struct Vec4: Codable {
     let w: Float
     let x: Float
     let y: Float
     let z: Float
 }
 
-struct InterspaceAnchor: Codable {
+// For decoding JSON text storing an anchor
+struct Anchor: Codable {
     let id: String
     let image_url: String
     let width_cm: Float
     let height_cm: Float
-    let position: Vector3
-    let orientation: Vector4
+    let position: Vec3
+    let orientation: Vec4
     let is_setup: Bool
     let is_origin: Bool
 }
 
+// class reponsible for all app functionality thus far
 class ViewController: UIViewController, ARSessionDelegate {
+    
+    // the object that is/manages the view for a RealityKit app
     @IBOutlet var arView: ARView!
     
+    // the object for configuring how the the ARSession operates
+    var arConfig = ARWorldTrackingConfiguration()
+    
+    // dictionary to associate each ARImageAnchor (ARKit) with a AnchorEntity (RealityKit)
     var imageAnchorToEntity: [ARImageAnchor: AnchorEntity] = [:]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        arView.session.delegate = self
-        let config = ARWorldTrackingConfiguration()
-        arView.automaticallyConfigureSession = false
-        config.sceneReconstruction = .meshWithClassification
-        arView.debugOptions.insert(.showSceneUnderstanding)
-        arView.session.run(config, options: [])
-        
-        var newReferenceImages: Set<ARReferenceImage> = Set<ARReferenceImage>()
-        
-        struct Response: Codable {
-            let anchors: [InterspaceAnchor]
-        }
-        
-        
-        if let url = URL(string: "http://10.100.0.204:8080/anchors") {
-            URLSession.shared.dataTask(with: url) { data, response, error in
-                if let data = data {
-                    do {
-                        let res = try JSONDecoder().decode(Response.self, from: data)
-                        print(res.anchors)
-                        for interspaceAnchor in res.anchors {
-                            print("anchor retrieved from Interspace")
-                            let url = URL(string: interspaceAnchor.image_url)!
-                            if let data = try? Data(contentsOf: url) {
-                                let downloadedImage = UIImage(data: data)
-                                let cgImage = downloadedImage?.cgImage
-                                let image = ARReferenceImage(cgImage!, orientation: CGImagePropertyOrientation.up, physicalWidth: CGFloat(interspaceAnchor.width_cm/100))
-                                newReferenceImages.insert(image)
-                            }
-                        }
-                        config.detectionImages = newReferenceImages
-                        print("Session rerun")
-                        self.arView.session.run(config)
-                    } catch let error {
-                        print(error)
-                    }
-                }
-           }.resume()
-        }
-        
-        config.detectionImages = newReferenceImages
-        print("Session rerun")
-        arView.session.run(config)
-        
-        // Set world origin to origin anchor's transform
-        // arView.session.setWorldOrigin(relativeTransform: anchor.transform)
-        
+    // the object that deals with downloading web data
+    let session = URLSession.shared
+    
+    // ask the Interspace for a list of anchors
+    func requestAnchors(baseUrl: URL) {
+        // create a URL object (using the base url of the Interspace with the path to get the anchor list)
+        let url = URL(string: "/anchors", relativeTo: baseUrl)!
+        // create a task which queries the url with a GET request, calling 'receiveAnchors' when it gets a reply
+        let task = session.dataTask(with: url, completionHandler: recieveAnchors)
+        // start the task (doesn't run in the main thread)
+        task.resume()
     }
     
-    
-    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
-        anchors.compactMap { $0 as? ARImageAnchor }.forEach {
-            
-            let anchorEntity = AnchorEntity()
-            let boxAnchor = try! Experience.loadBox()
-            let modelEntity = boxAnchor.steelBox!
-            anchorEntity.addChild(modelEntity)
-            arView.scene.addAnchor(anchorEntity)
-            anchorEntity.transform.matrix = $0.transform
-            imageAnchorToEntity[$0] = anchorEntity
-            // This is for the origin anchor
-            //self.arView.session.setWorldOrigin(relativeTransform: anchorEntity.transform)
+    // called by the task that was setup in the 'requestAnchors' function with GET request's reply
+    func recieveAnchors(data: Data?, response: URLResponse?, error: Error?) {
+        // create a struct for decoding the JSON data of the reply
+        struct DecodedData: Codable {
+            let anchors: [Anchor]
         }
-    }
-    
-    func getInterspaceAnchorArray() -> [InterspaceAnchor]{
-        struct Response: Codable {
-            let anchors: [InterspaceAnchor]
-        }
-        var anchor_list: [InterspaceAnchor] = []
-        
-        if let url = URL(string: "http://10.100.0.204:8080/anchors") {
-            URLSession.shared.dataTask(with: url) { data, response, error in
-                if let data = data {
-                    do {
-                        let res = try JSONDecoder().decode(Response.self, from: data)
-                        //return res.anchors
-                    } catch let error {
-                        print(error)
-                    }
-                }
-           }.resume()
-        }
-        return anchor_list
-    }
-    
-    func create_anchor() {
-        let position = Vector3(x: 0, y: 0, z: 0)
-        let orientation = Vector4(w: 0, x: 0, y: 0, z: 0)
-        let anchor = InterspaceAnchor(
-            id: UUID().uuidString,
-            image_url: "http://10.100.0.204:8080/image/anchor_03.jpg",
-            width_cm: 8.0,
-            height_cm: 8.0,
-            position: position,
-            orientation: orientation,
-            is_setup: true,
-            is_origin: true
-        )
-        guard let requestData = try? JSONEncoder().encode(anchor) else { return }
-        
-        let url = URL(string: "http://10.100.0.204:8080/create_anchor")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        struct Response: Codable {
-            let status: String
-        }
-        
-        URLSession.shared.uploadTask(with: request, from: requestData) { data, response, error in
-            if let data = data {
-                do {
-                    let res = try JSONDecoder().decode(Response.self, from: data)
-                    print(res.status)
-                } catch let error {
-                    print(error)
+        // if the reply's 'data' variable contains anything (also making it type 'Data' not 'Data?')
+        if let data = data {
+            // if the JSON text in 'data' decoded into an instance of the 'DecodedData' struct
+            if let decodedData = try? JSONDecoder().decode(DecodedData.self, from: data) {
+                // make loading the anchors happen in the main thread (im not certain the effect of not doing this)
+                DispatchQueue.main.async {
+                    // call function to load the anchors for detection
+                    self.loadAnchors(anchors: decodedData.anchors)
                 }
             }
-        }.resume()
+        }
+    }
+    
+    // load the images of each anchor into the images for the ARSession to detect
+    func loadAnchors(anchors: [Anchor]) {
+        // create an empty set of ARReferenceImage(s)
+        var detectionImages = Set<ARReferenceImage>()
+        // for each anchor, load the image it specifes (with a url) into an ARReferenceImage
+        for anchor in anchors {
+            // if an ARReferenceImage object is returned from getDetectionImage
+            if let detectionImage = getDetectionImage(anchor: anchor) {
+                // add it to the ARReferenceImage set
+                detectionImages.insert(detectionImage)
+            }
+        }
+        // make the images for the ARSession to detect the new ARReferenceImage set
+        arConfig.detectionImages = detectionImages
+        // reload (somehow without stopping) the ARSession, with its new images to detect
+        arView.session.run(arConfig)
+    }
+    
+    // load the image a passed anchor specifies (with a url) into an ARReferenceImage
+    func getDetectionImage(anchor: Anchor) -> ARReferenceImage? {
+        // create a URL object from the passed anchor's image url string
+        let url = URL(string: anchor.image_url)!
+        // create an object that stores the width in meters derived from the anchor's width in cm
+        let width = CGFloat(anchor.width_cm/100)
+        // define the orientation for the ARReferenceImage (this isnt the anchor's 3D orientation)
+        let orientation = CGImagePropertyOrientation.up
+        // if data was returned after downloading it from the url
+        if let data = try? Data(contentsOf: url) {
+            // if the downloaded data could be used to instance a UIImage object
+            if let imageObject = UIImage(data: data) {
+                // load the underlying image data (Apples format)
+                let image = imageObject.cgImage!
+                // create an ARReferenceImage instance, for use by the ARSession for detecting the anchor
+                let detectionImage = ARReferenceImage(image, orientation: orientation, physicalWidth: width)
+                // define the name for the ARReferenceImage to identify which anchor it belongs to
+                detectionImage.name = anchor.id
+                // return the ARReferenceImage instance
+                return detectionImage
+            }
+        }
+        // return empty if something failed getting the detectionImage
+        return nil
+    }
+    
+    // called when the view loads, this is where execution for us begins
+    override func viewDidLoad() {
+        // make sure what ever was using viewDidLoad before still can do its thing first
+        super.viewDidLoad()
+        // make the ViewController class where RealityKit/ARSession events end up
+        arView.session.delegate = self
+        // tell RealityKit not to configure the ARSession for us (full effect uncertain)
+        arView.automaticallyConfigureSession = false
+        // tell RealityKit to create a mesh while the ARSession runs using the LiDAR sensor
+        arConfig.sceneReconstruction = .meshWithClassification
+        // tell RealityKit to draw that mesh as a colourful wireframe over everything it sees
+        arView.debugOptions.insert(.showSceneUnderstanding)
+        // tell RealityKit to start the ARSession with the config so far
+        arView.session.run(arConfig)
+        // create a URL object with the base url where interspace-python-poc-server can be reached
+        let url = URL(string: "http://10.100.0.204:8080")!
+        // request the anchors from it (this will run a chain of functions, mostly out of the main thread)
+        requestAnchors(baseUrl: url)
+    }
+    
+    // function called whenever an ARAnchor is added (including when images are detected)
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        // for each ARAnchor that is an ARImageAnchor (a detected image)
+        anchors.compactMap { $0 as? ARImageAnchor }.forEach {
+            // create a RealityKit AnchorEntity instance
+            let anchorEntity = AnchorEntity()
+            // load an instance of a 'Box' from RealityKit, defined in the Experience.rcproject
+            let boxAnchor = try! Experience.loadBox()
+            // load an instance of the model entity from the 'Box'
+            let modelEntity = boxAnchor.steelBox!
+            // parent the model entity to the AnchorEntity instance
+            anchorEntity.addChild(modelEntity)
+            // add the AnchorEntity instance to the RealityKit scene
+            arView.scene.addAnchor(anchorEntity)
+            // set the AnchorEntity instances position & orientation to the ARImageAnchors
+            anchorEntity.transform.matrix = $0.transform
+            // add the AnchorEntity instance against the ARImageAnchor in the dictionary
+            imageAnchorToEntity[$0] = anchorEntity
+        }
     }
 }
+
